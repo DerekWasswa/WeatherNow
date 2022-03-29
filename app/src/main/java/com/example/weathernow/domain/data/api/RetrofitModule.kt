207feaff5f1d6a9ace96cf94abf28b4ea1d3c6e6
DerekWasswa <wasswadero@gmail.com>
2022-03-29 21:12:33 +0300

    change the retrofit module contents - add weather service api key to the request headers - change the base url to conform to request url params - change the data converter to kotlinx serialization converter

--- app/src/main/java/com/example/weathernow/domain/data/api/RetrofitModule.kt
+++ app/src/main/java/com/example/weathernow/domain/data/api/RetrofitModule.kt
@@ -4,8 +4,13 @@ import android.content.Context
 import com.example.weathernow.BuildConfig
 import com.example.weathernow.R
 import com.example.weathernow.domain.utils.Constants
+import com.example.weathernow.domain.utils.Constants.BASE_URL
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -13,15 +18,18 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
+@ExperimentalSerializationApi
 class RetrofitModule(context: Context) : BaseRetrofitModule(context) {
 
     private lateinit var retrofitModule : Retrofit
 
     override fun getApiServiceService() : ApiService {
+        val contentType = "application/json".toMediaType()
+        val converterFactory = Json.asConverterFactory(contentType)
         val gson : Gson = GsonBuilder().setLenient().create()
-
         val clientBuilder = OkHttpClient.Builder()
         val loggingInterceptor = HttpLoggingInterceptor()
+
         loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
         clientBuilder.addInterceptor(loggingInterceptor)
         clientBuilder.connectTimeout(60, TimeUnit.SECONDS)
@@ -31,16 +39,17 @@ class RetrofitModule(context: Context) : BaseRetrofitModule(context) {
             val requestBuilder = chain.request().newBuilder()
             requestBuilder.header("Content-Type", "application/json")
             requestBuilder.header("User-Agent", context.getString(R.string.app_name))
+            requestBuilder.header("appid", BuildConfig.OPEN_WEATHER_APIKEY)
             chain.proceed(requestBuilder.build())
         }
 
         clientBuilder.build()
 
         retrofitModule = Retrofit.Builder()
-            .baseUrl(BuildConfig.OPEN_WEATHER_APIKEY)
+            .baseUrl(BASE_URL)
             .client(clientBuilder.build())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addConverterFactory(converterFactory)
             .build()
 
 
